68474736a6f005b1c5f254935702f77bae21a13f
Jason Pitts <juniorjr083@gmail.com>
2018-08-04 02:29:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,13 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     current = Time.now 
-   current <= 12 ?  resp.write "Good Morning!" : resp.write ""
-     
-      
-      
-     
-    resp.write 
-    #resp.write "Hello, World"
+    current <= 12 ?  resp.write "Good Morning!" : resp.write "Good Afternoon!"
     resp.finish
   end 
   
