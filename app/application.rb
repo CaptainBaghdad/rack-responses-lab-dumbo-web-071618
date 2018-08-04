b6ec8d3c29ee2f73cafd0ea25f4de3769b81f7e5
Jason Pitts <juniorjr083@gmail.com>
2018-08-04 02:27:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,11 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    resp.write Time.new(2018) 
+    current = Time.now 
+    if current <= 12
+      
+    end 
+    resp.write 
     #resp.write "Hello, World"
     resp.finish
   end 
